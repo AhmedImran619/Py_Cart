73f4184ec4d5d01c21aa4b3dae9c9b664ce86286
Ahmed <ahmed.flutter@magnatectsystems.com>
2021-07-08 16:11:17 +0500

    started working on shop app and layed the pipe

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='ShopHome'),
+    path('cart/', views.cart, name='CartPage'),
+    path('categories/', views.categories, name='CategoriesPage'),
+    path('search/', views.search, name='SearchPage'),
+    path('about/', views.about, name='AboutPage'),
+
+    path('product_list/', views.product_list, name='ProductListPage'),
+    path('product_detail/', views.product_detail, name='ProductDetailPage'),
+
+    path('login/', views.login, name='LoginPage'),
+    path('register/', views.register, name='RegisterPage'),
+
+    path('account/', views.account, name='AccountPage'),
+    path('account_detail/', views.account_detail, name='AccountDetailPage'),
+    path('favorites/', views.favorites, name='FavoritesPage'),
+    path('orders/', views.orders, name='OrdersPage'),
+    path('order_detail/', views.order_detail, name='OrderDetailPage'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,61 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def cart(request):
+    return HttpResponse('Cart Page')
+
+
+def categories(request):
+    return HttpResponse('Categories Page')
+
+
+def search():
+    return HttpResponse('Search Page')
+
+
+def about():
+    return HttpResponse('About Page')
+
+
+# ------------------------------------------------
+
+def product_list():
+    return HttpResponse('Product List Page')
+
+
+def product_detail():
+    return HttpResponse('Product Detail Page')
+
+
+# ------------------------------------------------
+
+def login():
+    return HttpResponse('Login Page')
+
+
+def register():
+    return HttpResponse('Register Page')
+
+
+# ------------------------------------------------
+
+def account():
+    return HttpResponse('Account Page')
+
+
+def account_detail():
+    return HttpResponse('Account Detail Page')
+
+
+def favorites():
+    return HttpResponse('Favorites Page')
+
+
+def orders():
+    return HttpResponse('Order Page')
+
+
+def order_detail():
+    return HttpResponse('Order Detail Page')
