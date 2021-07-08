5ab262d3d5860858b51d3de416084ed54d450d23
Ahmed <ahmed.flutter@magnatectsystems.com>
2021-07-08 16:58:02 +0500

    - created a product model (table for db) in models.py - created a migration for that model - apply that migration - added to ShopConfig in settings.py INSTALLED_APPS

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    date_added = models.DateField()
