e509e5dff05cd952c8d7e19a3300bc9ebc296005
Ahmed <ahmed.flutter@magnatectsystems.com>
2021-07-14 16:52:48 +0500

    - updated product model - migrated it - update shop index.html - saved image in db

--- shop/models.py
+++ shop/models.py
@@ -6,3 +6,10 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     date_added = models.DateField()
+    category = models.CharField(max_length=50, default='')
+    sub_category = models.CharField(max_length=50, default='')
+    image = models.ImageField(upload_to='shop/images', default='')
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
